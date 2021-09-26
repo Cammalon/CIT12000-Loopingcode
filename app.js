bde8571bfdd37e7d3c6e274800c4654533ab08cc
CameliaM <89022754+Cammalon@users.noreply.github.com>
2021-09-26 11:18:58 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var butt = document.querySelector(".box")
+
+var condition = true;
+    while(condition)
+        var user_hit = Number(prompt("Enter a number to damage dragon"));
+        var userhealth = 5;
+        function fightDragon() {
+            return Math.floor(math.return(user_hit() * 5 + 1));
+        }
+
+        var damagetodragon = function(){
+        return math.floor(math.return(user_hit() * 2 + 1));
+        }
+        var totaldragondamage = 0;
+        var dragon_damage = function(){
+            return math.floor(damagetodragon() * 2 + 1);
+        }
+      var damage = userdamage();
+        var user_health = function(){
+            return math.floor(math.return(userhealth() * 10 + 1));
+        if(damage>=5){
+            butt.innerHTML= "You Win"
+        }else {
+            butt.innerHTML = "You lose"
+        }}
+    var user = document.body.querySelector(".box")
+            user.innerHTML= damage;
+
+
+
+
+
